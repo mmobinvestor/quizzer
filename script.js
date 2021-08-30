7210cfc1ee16ff009f1e0b920c94dc41df3968e9
mmobinvestor <s.setevoy@gmail.com>
2021-08-31 01:02:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 // ==UserScript==
 // @name         Quiz Checker
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      0.1
 // @description  try to take over the world!
-// @author       Jonh Doe
+// @author       George
 // @match        https://coinlist.co/*
-// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @icon         https://www.google.com/s2/favicons?domain=coinlist.co
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @updateURL    https://raw.githubusercontent.com/mmobinvestor/quizzer/main/script.js
 // @downloadURL  https://raw.githubusercontent.com/mmobinvestor/quizzer/main/script.js
-// @grant        GM_getResourceText
-// @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -25,20 +23,32 @@
 
     if (path.includes('/new')) {
         setTimeout(function() {
-            //$('.js-offerings_participant_form').trigger('submit');
-
-            console.log($('.js-offerings_participant_form'));
+            $('.js-submit_existing_entity')[0].click();
         }, 500);
     }
 
+    if (path.includes('/residence')) {
+        setTimeout(function() {
+            $('.js-country option[value="RU"]').prop('selected', true).change();
+            $('#forms_offerings_participants_residence_residence_signature').prop('checked', true).change();
+            $('.js-submit')[0].click();
+        }, 500);
+    }
+
+    // Мы на странице опроса
     if (path.includes('/quiz')) {
-        $.getJSON('https://www.seomax.info/answers?url=' + path, function(answers) {
-            for (var i = 0; i < answers.length; ++i) {
-                $('label:contains("'+answers[i]+'")').find('input').prop('checked', true).trigger('change');
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'https://www.seomax.info/answers?url=/' + path.split('/')[1],
+            onload: function (response) {
+                var answers = JSON.parse(response.responseText);
+
+                for (var i = 0; i < answers.length; ++i) {
+                    $('label:contains("'+answers[i]+'")').find('input').prop('checked', true).trigger('change');
+                }
             }
         });
-
-        $('.js-submit')[0].click();
+        // $('.js-submit')[0].click();
     }
 
 })();
